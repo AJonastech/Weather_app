a7ecc9309c21fd5aaab47dafed38c705a366996f
AJonastech <113301012+AJonastech@users.noreply.github.com>
2022-09-29 01:25:44 +0100

    Commited this projec
    My new project

---
+++ script.js
@@ -0,0 +1,9 @@
+function Darkmode(){
+    document.getElementById("light-mode").style.display="none";
+    document.getElementById("dark-mode").style.display="block";
+}
+function lightmode(){
+    document.getElementById("light-mode").style.display="block";
+    document.getElementById("dark-mode").style.display="none";
+   
+}
